031b282f182b15a9943ef38eaff326f68b60ecd4
Danilo Jesús <danilojesus112001@gmail.com>
2026-02-12 08:19:36 -0300

    feat: add hello world view with test

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views import generic
+
+class PostView(generic.View):
+  def get(self, request, *args, **kwargs):
+    return HttpResponse("Hello World")
