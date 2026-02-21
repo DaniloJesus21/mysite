62be1f5023c8cf543c3b79ff5168d9d48bdb52a8
Danilo Jesús <danilojesus112001@gmail.com>
2026-02-21 16:30:28 -0300

    feat: register Post model in django admin

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+class PostAmin(admin.ModelAdmin):
+  list_display = ("title", "slug", "author", "created_at", "status")
+  list_filter = ('status',)
+  search_fields = ["title", "content"]
+  prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Post, PostAmin)
